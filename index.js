e7314b08804ad1787e0f9d03fc83a0d585e3297f
Kyle Groening <kylegroening@gmail.com>
2023-03-05 13:34:45 -0500

    Add CORS headers to response object

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const http = require('http')
-const mongoose = require('mongoose')
 const port = require('./utils/getPort')()
-
+const routes = require('./routes')
 require('dotenv').config()
-
+require('./utils/dbConnect')();
 const app = express()
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+})
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
 app.use('/api/posts', routes.posts);
