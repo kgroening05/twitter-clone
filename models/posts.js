511e47537ef8052f9873d0dbaff60d5106caead4
Kyle Groening <kylegroening@gmail.com>
2023-03-05 13:35:34 -0500

    Remove title field, fix User field

--- models/posts.js
+++ models/posts.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    title: { type: String, required: true },
     body: { type: String, required: true},
-    author: { type: Schema.Types.ObjectId, ref:"users", required : true },
+    author: { type: Schema.Types.ObjectId, ref:"User", required : true },
     postOn: { type: Date, default: Date.now }
 },{
     collection: 'posts',
