94f3363c8592e48cdc2bc2f03140185b9937ff1c
Kyle Groening <kylegroening@gmail.com>
2023-03-12 18:05:41 -0400

    Add routes for google auth

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 const User = require('../models/users');
 const verifyToken = require('../utils/verifyToken')
+const passport = require('../utils/passportStrategies')
+
 require('dotenv').config()
 
 router.get(
@@ -28,6 +30,11 @@ router.get(
     (req, res) => {
         res.json({ token: req.token})
     }
+)
+
+router.get(
+    '/google-auth',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
 )
 
 function verifyUser(req, res, next) {
