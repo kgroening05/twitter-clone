fae0c9ea027319a39e9f92e1f9a91f86a0548c63
Kyle Groening <kylegroening@gmail.com>
2023-03-12 18:05:56 -0400

    Add session connection

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 require('dotenv').config()
 
@@ -10,11 +12,25 @@ const password = process.env.DBPASSWORD;
 const database = process.env.DATABASE;
 const cluster = process.env.CLUSTER;
 
+// store secrets
+const collection = process.env.SESSIONCOLLECTION
+
 // connection string
 const mongoDb = `mongodb+srv://${username}:${password}@${cluster}.fsq1lj2.mongodb.net/${database}?` 
                 + `retryWrites=true&`
                 + `w=majority`;
  
-module.exports = async function (){
+async function connectDb(){
     await mongoose.connect(mongoDb)
 }
+
+const store = new MongoDBStore({
+    uri: mongoDb,
+    collection: collection
+});
+
+store.on('error', (err) => {
+    console.log(err)
+})
+
+module.exports = { connectDb, store }
