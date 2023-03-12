d9d6321a496ce0679b59335d19f5af710d357879
Kyle Groening <kylegroening@gmail.com>
2023-03-12 18:04:22 -0400

    Add session and passport middleware

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const http = require('http')
 const port = require('./utils/getPort')()
 const routes = require('./routes')
+const passport = require('./utils/passportStrategies')
+const session = require("express-session");
+const { store, connectDb } = require('./utils/dbConnect')
+
+connectDb()
 require('dotenv').config()
-require('./utils/dbConnect')();
 const app = express()
 
 app.use(function (req, res, next) {
@@ -14,9 +18,22 @@ app.use(function (req, res, next) {
 })
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
+app.use(session({
+    secret: process.env.SESSIONSECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+    },
+    store: store,
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/api/posts', routes.posts);
 app.use('/api/users', routes.users);
 app.use('/api/login', routes.login);
+app.use('/callback', routes.authCallback);
 
 const server = http.createServer(app)
 server.listen(port)
